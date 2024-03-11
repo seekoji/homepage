b562d1675a205a074e4e00bf714fc20f2969c262
seekoji <99361556+seekoji@users.noreply.github.com>
2024-03-11 19:22:57 +0300

    last fix

--- src/app/components/theme_switcher.tsx
+++ src/app/components/theme_switcher.tsx
@@ -1,45 +1,66 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { SunIcon, MoonIcon } from "./icons";
+import { AnimatePresence, motion } from "framer-motion";
 
-export default function ThemeSwitcher() {
+function ThemeSwitcher() {
+	const storedTheme = window.localStorage.getItem("preferred-theme");
 	const checkTheme = () => {
-		const storedTheme = window.localStorage.getItem("prefered-theme");
-		if (storedTheme === "darkTheme") {
-			return true;
-		}
-		if (
-			window.matchMedia &&
-			window.matchMedia("(prefers-color-scheme: dark)").matches
-		) {
+		if (storedTheme === "lightTheme") {
 			return true;
 		}
 		return false;
 	};
-
 	const [isLight, setIsLight] = useState(checkTheme());
-
+	function setLightTheme() {
+		setIsLight(true);
+		window.localStorage.setItem("preferred-theme", "lightTheme");
+	}
+	function setDarkTheme() {
+		setIsLight(false);
+		window.localStorage.setItem("preferred-theme", "darkTheme");
+	}
 	useEffect(() => {
-		const root = window.document.documentElement;
-		if (isLight) {
-			root.removeAttribute("class");
-		} else {
-			root.classList.add("dark");
-		}
+		const setTheme = () => {
+			const root = window.document.documentElement;
+			if (isLight) {
+				root.classList.remove("dark");
+			} else {
+				root.classList.add("dark");
+			}
+		};
+		setTheme();
 	}, [isLight]);
 
-	const setLightTheme = () => {
-		setIsLight(true);
-		window.localStorage.setItem("prefered-theme", "lightTheme");
-	};
-
-	const setDarkTheme = () => {
-		setIsLight(false);
-		window.localStorage.setItem("prefered-theme", "darkTheme");
-	};
-
 	return (
-		<div className="theme-switcher">
-			<button onClick={setLightTheme}>Light theme</button>
-			<button onClick={setDarkTheme}>Dark theme</button>
-		</div>
+		<AnimatePresence mode="wait" initial={false}>
+			<motion.div
+				key={isLight ? "light" : "dark"}
+				initial={{ y: -20, opacity: 0 }}
+				animate={{ y: 0, opacity: 1 }}
+				exit={{ y: 20, opacity: 0 }}
+				transition={{ duration: 0.2 }}
+			>
+				<div className="items-center">
+					<button
+						type="button"
+						className={`dark-mode-switch cursor-pointer w-12 h-10
+        ${isLight ? "hidden" : ""}`}
+						onClick={setDarkTheme}
+					>
+						<MoonIcon />
+					</button>
+					<button
+						type="button"
+						className={`light-mode-switch cursor-pointer w-12 h-10
+         ${isLight ? "" : "hidden"}`}
+						onClick={setLightTheme}
+					>
+						<SunIcon />
+					</button>
+				</div>
+			</motion.div>
+		</AnimatePresence>
 	);
 }
+
+export default ThemeSwitcher;
