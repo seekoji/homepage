155a6e45f39d854c9600bd578f2099a5762aa357
seekoji <99361556+seekoji@users.noreply.github.com>
2024-03-28 16:42:48 +0300

    fixes, added input component

--- src/app/components/icons.tsx
+++ src/app/components/icons.tsx
@@ -99,28 +99,6 @@ export function SteamIcon(): any {
 	);
 }
 
-export function ArrowLeft(): any {
-	return (
-		<svg
-			className="--svg-icon"
-			width="24px"
-			height="24px"
-			strokeWidth="2"
-			viewBox="0 0 24 24"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-			color="#ffff"
-		>
-			<path
-				d="M15 6L9 12L15 18"
-				strokeWidth="2"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			></path>
-		</svg>
-	);
-}
-
 export function TwitchIcon(): any {
 	return (
 		<svg
@@ -136,90 +114,20 @@ export function TwitchIcon(): any {
 	);
 }
 
-export function MoonIcon(): any {
+export function ArrowLeft(): any {
 	return (
 		<svg
 			className="--svg-icon"
 			width="24px"
 			height="24px"
-			strokeWidth="1.5"
+			strokeWidth="2"
 			viewBox="0 0 24 24"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
-			color="#000000"
+			color="#ffff"
 		>
 			<path
-				d="M3 11.5066C3 16.7497 7.25034 21 12.4934 21C16.2209 21 19.4466 18.8518 21 15.7259C12.4934 15.7259 8.27411 11.5066 8.27411 3C5.14821 4.55344 3 7.77915 3 11.5066Z"
-				strokeWidth="2"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			></path>
-		</svg>
-	);
-}
-
-export function SunIcon(): any {
-	return (
-		<svg
-			className="--svg-icon"
-			width="24px"
-			height="24px"
-			strokeWidth="1.5"
-			viewBox="0 0 24 24"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-			color="#000000"
-		>
-			<path
-				d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18Z"
-				strokeWidth="2"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			></path>
-			<path
-				d="M22 12L23 12"
-				strokeWidth="2"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			></path>
-			<path
-				d="M12 2V1"
-				strokeWidth="2"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			></path>
-			<path
-				d="M12 23V22"
-				strokeWidth="2"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			></path>
-			<path
-				d="M20 20L19 19"
-				strokeWidth="2"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			></path>
-			<path
-				d="M20 4L19 5"
-				strokeWidth="2"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			></path>
-			<path
-				d="M4 20L5 19"
-				strokeWidth="2"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			></path>
-			<path
-				d="M4 4L5 5"
-				strokeWidth="2"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			></path>
-			<path
-				d="M1 12L2 12"
+				d="M15 6L9 12L15 18"
 				strokeWidth="2"
 				strokeLinecap="round"
 				strokeLinejoin="round"
